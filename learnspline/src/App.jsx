fb0e996d77db1d810c2ccdbe9d652b307a6c3c7c
Rohit-Singh-07 <150457028+Rohit-Singh-07@users.noreply.github.com>
2024-04-21 20:04:03 +0530

    commit

--- learnspline/src/App.jsx
+++ learnspline/src/App.jsx
@@ -1,19 +1,51 @@
+import React, { useState, useEffect } from "react";
 import Spline from "@splinetool/react-spline";
+import { motion } from "framer-motion"
+import Lottie from "lottie-react";
+import infinityLoading from "./assets/rgbCircleLoading.json"
 
 export default function App() {
+ 
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    // Simulate loading for 5 seconds
+    const timeout = setTimeout(() => {
+      setLoaded(true);
+      setLoading(false);
+    }, 5000);
+
+    // Clear the timeout when component unmounts
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-    <>
-      <Spline scene="https://prod.spline.design/oSgXtN1Tp7JKSMSE/scene.splinecode" />
+    <div style={{ position: "relative", width: "100%", height: "100%" }}>
+      {loaded===false ? 
+      <motion.div 
+      initial={{ opacity: 1 }}
+      animate={{ opacity: 0 }}
+      transition={{duration: 5, delay: 2}}
+      className="absolute top-0 bg-transparent backdrop-blur-md h-[100vh] w-[98vw] flex justify-center items-center">
+        <Lottie className="h-[50vh]" animationData={infinityLoading} loop={true} />;
+        </motion.div>: <div></div>}
+      <div>
+        <Spline scene="https://prod.spline.design/oSgXtN1Tp7JKSMSE/scene.splinecode" />
 
-      <Spline scene="https://prod.spline.design/WSSp1QMwth8v54nX/scene.splinecode" />
+        <div style={{ visibility: loaded ? "visible" : "hidden" }}>
+          <Spline scene="https://prod.spline.design/WSSp1QMwth8v54nX/scene.splinecode" />
 
-      <Spline scene="https://prod.spline.design/wQSefpZ8cKUTamvS/scene.splinecode" />
+          <Spline scene="https://prod.spline.design/wQSefpZ8cKUTamvS/scene.splinecode" />
 
-      <Spline scene="https://prod.spline.design/BQqcEv7Mda6DC-zM/scene.splinecode" />
+          <Spline scene="https://prod.spline.design/BQqcEv7Mda6DC-zM/scene.splinecode" />
 
-      <Spline scene="https://prod.spline.design/Hb6B9PfNAomrRDDh/scene.splinecode" />
+          <Spline scene="https://prod.spline.design/Hb6B9PfNAomrRDDh/scene.splinecode" />
 
-      <div className="h-[50vh] flex justify-center items-center text-xl">filler</div>
-    </>
+          <div className="h-[50vh] flex justify-center items-center text-xl">
+            filler
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
