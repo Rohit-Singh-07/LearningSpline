0fd7002310b52f9fab3c283e29014bcd5f839fe0
Rohit-Singh-07 <150457028+Rohit-Singh-07@users.noreply.github.com>
2024-04-21 17:13:37 +0530

    commit

--- learnspline/src/App.jsx
+++ learnspline/src/App.jsx
@@ -3,9 +3,9 @@ import Spline from "@splinetool/react-spline";
 export default function App() {
   return (
     <>
-      <Spline scene="https://prod.spline.design/WSSp1QMwth8v54nX/scene.splinecode" />
-
       <Spline scene="https://prod.spline.design/oSgXtN1Tp7JKSMSE/scene.splinecode" />
+
+      <Spline scene="https://prod.spline.design/WSSp1QMwth8v54nX/scene.splinecode" />
 
       <Spline scene="https://prod.spline.design/wQSefpZ8cKUTamvS/scene.splinecode" />
 
@@ -13,9 +13,7 @@ export default function App() {
 
       <Spline scene="https://prod.spline.design/Hb6B9PfNAomrRDDh/scene.splinecode" />
 
-      <div className="h-[50vh] flex justify-center items-center text-xl">
-        filler
-      </div>
+      <div className="h-[50vh] flex justify-center items-center text-xl">filler</div>
     </>
   );
 }
